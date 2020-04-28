83df999730c92c1903bc54c56478c45170df220b
Igor Maliy <imaliy.filsh@gmail.com>
2020-04-28 10:29:54 +0300

    Initial commit

--- views/default/edit.php
+++ views/default/edit.php
@@ -14,9 +14,9 @@ $this->setTitle('Edit Brand #' . $brand->getId());
 ?><div class="row">
     <div class="col-12">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-            <h1 class="h2">Edit Brand #<?= $brand->getId() ?></h1>
+            <h1 class="h2">Edit brand #<?= $brand->getId() ?></h1>
             <div class="btn-toolbar float-right">
-                <a class="btn btn-sm btn-info mx-sm-1 mb-2" href="<?= $urlGenerator->generate('/brand/default/index', ['id' => $brand->getId()]) ?>">
+                <a class="btn btn-sm btn-info mx-sm-1 mb-2" href="<?= $urlGenerator->generate('/dashboard/default/index', ['brandId' => $brand->getId()]) ?>">
                     <?= Icon::widget()->name('eye')->options(['class' => 'mr-1']); ?>
                     View
                 </a>
@@ -29,7 +29,7 @@ $this->setTitle('Edit Brand #' . $brand->getId());
 </div>
 <div class="mb-2"></div>
 <div class="row">
-    <div class="col-12">
-        <?= (new FormRenderer())($brandForm, $submitted) ?>
+    <div class="col-6">
+        <?= (new FormRenderer($brandForm))($submitted) ?>
     </div>
 </div>
